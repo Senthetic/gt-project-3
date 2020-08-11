6e4b4e5dce8b8b41c53f2af1d794037a42a5e921
binjim2002 <binjim2002@yahoo.fr>
2020-08-10 20:13:24 -0400

    creating a drink category route

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-    Plans: require('./plans.js')
+    Plans: require('./plans.js'),
+    DrinkCategories: require('./drinkCategory.js')
 }
--- server.js
+++ server.js
@@ -37,6 +37,8 @@ mongoose
   });
 
 
+// seed
+require('./backend/seeds.js')
 
 app.listen(PORT, () => {
   console.log(`Express server running on http://localhost:${PORT}`);
