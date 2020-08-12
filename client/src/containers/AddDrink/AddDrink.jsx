0c286b65397a6a642ab3289b87c05e460dc8d501
Brian Cook <bcrazorrider@gmail.com>
2020-08-12 13:33:16 -0400

    adding the section to add drink name

--- client/src/containers/AddDrink/AddDrink.jsx
+++ client/src/containers/AddDrink/AddDrink.jsx
@@ -4,50 +4,63 @@ import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      "& .MuiTextField-root": {
-        margin: theme.spacing(1),
-        width: "25ch",
-      },
-    },
-    button: {
-      fontSize: 30,
-    },
-    formControl: {
+  root: {
+    "& .MuiTextField-root": {
       margin: theme.spacing(1),
-      minWidth: 120,
+      width: "25ch",
     },
-    selectEmpty: {
-      marginTop: theme.spacing(2),
-    },
-  }));
+  },
+  button: {
+    fontSize: 30,
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 const AddDrink = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-  
-    const handleChange = (event) => {
 
-      console.log("");
-    };
+  const handleChange = (event) => {
+
+    console.log("");
+  };
+
+  return (
+    <div>
+      <h1>Blackout Preventer</h1>
   
-    return (
-        <FormControl className={classes.formControl}>
-          <InputLabel id="demo-simple-select-label">Type</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={""}
-            onChange={handleChange}
-          >
-            <MenuItem value={5}>Beer</MenuItem>
-            <MenuItem value={12}>Wine</MenuItem>
-            <MenuItem value={40}>Liquor</MenuItem>
-            </Select>
-            </FormControl>
+    <FormControl className={classes.formControl}>
+      <InputLabel id="demo-simple-select-label">Type</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={""}
+        onChange={handleChange}
+      >
+        <MenuItem value={5}>Beer</MenuItem>
+        <MenuItem value={12}>Wine</MenuItem>
+        <MenuItem value={40}>Liquor</MenuItem>
+      </Select>
+      <TextField
+        required
+        id="outlined-basic"
+        label="Name of drink"
+        type="string"
+        helperText="ex: Bud Light"
+      />
+    </FormControl>
+    </div>
     );
 
-    };
-    export default AddDrink;
+};
+export default AddDrink;
