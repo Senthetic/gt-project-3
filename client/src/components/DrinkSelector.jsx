e14f292015c4f772e3f808463b8692969b9750f4
Andrew Collier <atc3598@icloud.com>
2020-08-12 11:42:43 -0400

    added changes to TextField, there was a type in the import in DrinkSelector.jsx

--- client/src/components/DrinkSelector.jsx
+++ client/src/components/DrinkSelector.jsx
@@ -1,37 +1,38 @@
-import React, {useState, useEffect} from 'react';
-import Autocomplete from '@material-ui/lab/autocomplete';
-import TextField from "@material-ui/core/textfield";
-import Api from "../utils/api"
+import React, { useState, useEffect } from "react";
+import Autocomplete from "@material-ui/lab/autocomplete";
+import TextField from "@material-ui/core/TextField";
+import Api from "../utils/api";
 
+const DrinkSelector = ({ onSelected, variant }) => {
+  const [options, setOptions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [drink, setDrink] = useState({});
+  const [loading, setLoading] = useState(false);
 
-const DrinkSelector = ({onSelected, variant}) => {
-    const [options, setOptions] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [drink, setDrink] = useState({});
-    const [loading, setLoading] = useState(false);
-      
-    useEffect(()=>{
-        if(searchTerm.length > 3){
-            setLoading(true)
-            Api.get('/drinks/search?q='+searchTerm).then(res => {
-                setOptions(res.data);
-                console.log(options)
-                setLoading(false)
-            })
-        }
-        
-    },[searchTerm])
-    return (
-        <>
-        <Autocomplete 
+  useEffect(() => {
+    if (searchTerm.length > 3) {
+      setLoading(true);
+      Api.get("/drinks/search?q=" + searchTerm).then((res) => {
+        setOptions(res.data);
+        console.log(options);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+  return (
+    <>
+      <Autocomplete
         loading={loading}
-        renderInput={(params) => <TextField {...params} label="Drink selector" variant={variant} />}
-        getOptionSelected={setDrink} 
+        renderInput={(params) => (
+          <TextField {...params} label="Drink selector" variant={variant} />
+        )}
+        getOptionSelected={setDrink}
         getOptionLabel={(option) => option.name}
-        options={options} 
-        onInputChange={(ev,val) => setSearchTerm(val)}/>
-        </>
-    );
+        options={options}
+        onInputChange={(ev, val) => setSearchTerm(val)}
+      />
+    </>
+  );
 };
 
 export default DrinkSelector;
