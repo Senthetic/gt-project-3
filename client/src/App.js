36b2661aa5fbf9dbf72d9f58931c2561a476b94c
Brian Cook <bcrazorrider@gmail.com>
2020-08-12 12:46:57 -0400

    Brian develope

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,11 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./containers/Home/Home";
 import NoMatch from "./containers/NoMatch/NoMatch";
 import NewPlan from "./containers/NewPlan/NewPlan";
+<<<<<<< HEAD
 import AddDrink from "./containers/AddDrink/AddDrink";
+=======
+import ViewPlans from "./containers/ViewPlans/ViewPlans"
+>>>>>>> b52d2d4a51605a3cb241113f5e1c9cdeefddf6f6
 // import { makeStyles } from "@material-ui/core/styles"
 // import TextField from '@material-ui/core/TextField';
 
@@ -38,7 +42,11 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/NewPlan" component={NewPlan} />
+<<<<<<< HEAD
         <Route exact path="/AddDrink" component={AddDrink} />
+=======
+        <Route exact path="/ViewPlans" component={ViewPlans} />
+>>>>>>> b52d2d4a51605a3cb241113f5e1c9cdeefddf6f6
         <Route component={Home} />
       </Switch>
     </Router>
