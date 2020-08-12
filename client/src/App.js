3152af5901973963e5f2a735d39f4fcbf6a27593
Andrew Collier <atc3598@icloud.com>
2020-08-12 12:45:25 -0400

    cleaned up app.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,8 @@ import Home from "./containers/Home/Home";
 import NoMatch from "./containers/NoMatch/NoMatch";
 import NewPlan from "./containers/NewPlan/NewPlan";
 import ViewPlans from "./containers/ViewPlans/ViewPlans"
-// import { makeStyles } from "@material-ui/core/styles"
-// import TextField from '@material-ui/core/TextField';
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     '& .MuiTextField-root': {
-//       margin: theme.spacing(1),
-//       width: '25ch',
-//     },
-//   },
-// }));
-
-// export default function FormPropsTextFields() {
-//   const classes = useStyles();
-
 
 function App() {
-  // const classes = useStyles();
   useEffect(() => {
     axios
       .get("/api/config")
