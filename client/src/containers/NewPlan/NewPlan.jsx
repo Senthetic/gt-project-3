1879bfec068230b2c2906d4f438214eae58c46e8
Andrew Collier <atc3598@icloud.com>
2020-08-12 11:15:56 -0400

    added basic layout for add plan page, adding time box now

---
+++ client/src/containers/NewPlan/NewPlan.jsx
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import EditIcon from "@material-ui/icons/Edit";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+import DrinkSelector from "../../components/DrinkSelector";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "25ch",
+      flexGrow: 1,
+    },
+  },
+  button: {
+    fontSize: 30,
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const Home = () => {
+  const classes = useStyles();
+  const [abv, setAbv] = React.useState("");
+  // const [bac, setBac] = React.useState('');
+  // const [ounces, setOunces] = React.useState('');
+  // const [weight, setWeight] = React.useState('');
+  // const [hours, setHours] = React.useState('');
+
+  const handleChange = (event) => {
+    console.log(abv);
+  };
+
+  return (
+    <>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <form className={classes.root} noValidate autoComplete="off">
+              <TextField
+                id="outlined-basic"
+                label="Edit Plan Name"
+                variant="outlined"
+              />
+            </form>
+          </Grid>
+          <Grid
+            container
+            direction="column"
+            justify="flex-start"
+            alignItems="flex-start"
+            item
+            xs={1}
+          >
+            <Fab color="primary" aria-label="add">
+              <AddIcon />
+            </Fab>
+          </Grid>
+          <Grid
+            container
+            direction="column"
+            justify="flex-start"
+            alignItems="flex-start"
+            item
+            xs={1}
+          >
+            
+            <Fab color="secondary" aria-label="add">
+              <AddIcon />
+            </Fab>
+            <Fab color="secondary" aria-label="add">
+              <AddIcon />
+            </Fab>
+          </Grid>
+
+          <Grid
+            container
+            direction="column"
+            justify="flex-start"
+            alignItems="flex-start"
+            item
+            xs={6}
+          >
+            <Paper className={classes.paper}>Bud Light</Paper>
+            <Paper className={classes.paper}>Scofflaw Basement</Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </>
+  );
+};
+
+export default Home;
