ea64bf8cafc852bd98df7e3bd8d6155b64f5e480
binjim2002 <binjim2002@yahoo.fr>
2020-08-12 20:21:50 -0400

    view plans functionality

--- client/src/containers/ListPlans/ListPlans.jsx
+++ client/src/containers/ListPlans/ListPlans.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 import Api from '../../utils/api'
 import {Link} from 'react-router-dom'
 import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      "& .MuiTextField-root": {
+        margin: theme.spacing(1),
+        width: "25ch",
+        flexGrow: 1,
+      },
+    },
+    
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+    },
+  }));
 
 const ListPlans = () => {
+    const classes = useStyles();
     const [plans, setPlans] = React.useState([])
     React.useEffect(()=>{
         Api.get('/plans').then(plans => {
@@ -14,7 +32,7 @@ const ListPlans = () => {
         <div>
             {plans.map(plan => (
                 <Link to={'editPlan/'+plan._id}>
-                    <Paper>{plan.name}</Paper>
+                    <Paper className={classes.paper}>{plan.name}</Paper>
                 </Link>
             ))}
             
