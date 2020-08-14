ae1c2a5199ce91501ea925b41b6ef089f2bf2581
Brian Cook <bcrazorrider@gmail.com>
2020-08-14 11:14:06 -0400

    adding the Drawer component to the New Plan and login page

--- client/src/containers/Login/Login.jsx
+++ client/src/containers/Login/Login.jsx
@@ -5,6 +5,9 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+
+import Drawer from "../../components/Drawer";
+
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
