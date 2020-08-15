eaaddd16f9b8572908983c21fda0ee1d0000efef
binjim2002 <binjim2002@yahoo.fr>
2020-08-14 20:39:58 -0400

    passport and bcrypt installation

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     Plans: require('./plans.js'),
-    DrinkCategories: require('./drinkCategory.js')
+    DrinkCategories: require('./drinkCategory.js'),
+    Users: require('.user.js')
 }
---
+++ backend/routes/authentication.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const models = require('../models');
+const bcrypt = require('bcrypt');
+
+ 
+
+router.post('/signup', (req, res) => {
+    bcrypt.ha
+})
