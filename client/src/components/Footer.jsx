63302e4cea5c90e40916a8c2815e04cdd594ef7f
Andrew Collier <atc3598@icloud.com>
2020-08-17 14:02:42 -0400

    added Disclaimer to the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -16,6 +16,7 @@ const Footer = () => {
         className="navbar fixed-bottom stickyFooter navbar-expand-lg"
         style={footerStyle}
       >
+        <h1>Disclaimer: By accessing or using the Blackout Preventer App or any page of this app, you signify your assent to this disclaimer. The contents of this app, including without limitation, all data, information, text, graphics, links, and other materials are provided as a convenience to our app users and are meant to be used for informational purposes only. We do not take responsibility for decisions taken by the reader based solely on the information provided in this app </h1>
         <div className="navbar-brand">© 2020 Brian Cook, Jimmy Kazadi, Andrew Collier</div>
       </nav>
     </footer>
