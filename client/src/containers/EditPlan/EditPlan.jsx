99c04174896069437b7e4909972d0d4994e01bfc
Andrew Collier <atc3598@icloud.com>
2020-08-17 12:59:22 -0400

    created my own calculate method using switch case instead of if statements, on top of that I added in demo resultMessages that still apply

--- client/src/containers/EditPlan/EditPlan.jsx
+++ client/src/containers/EditPlan/EditPlan.jsx
@@ -120,22 +120,34 @@ const EditPlan = (props) => {
     //GAC = total alcohol consumed in grams (total vol of all drinks)^^^ *
     // result = (GAC/(Body Weight grams x r)) * 100
     result = abvResults / weight - timeSlot * 0.015;
-    if (result < 0) {
-      resultMessage =
-        "You are at the only safe driving limit and are not legally intoxicated.";
-      console.log("-- neglible amount --");
-    } else {
-      if (result === "NaN") resultMessage = "Please try again.";
-      if (result < 0.03)
-        resultMessage =
-          "You feel mildly relaxed and maybe a little lightheaded. Your inhibitions are slightly loosened, and whatever mood you were in before you started drinking may be mildly intensified";
-      if (result < 0.06)
-        resultMessage =
-          "You feel warm and relaxed. If you're the shy type when you're sober, you lose your feelings of shyness. Your behavior may become exaggerated, making you talk louder or faster or act bolder than usual. Emotions are intensified, so your good moods are better and your bad moods are worse. You may also feel a mild sense of euphoria.";
-      if (result > 0.14)
-        resultMessage =
-          "You would be considered legally intoxicated in all or most states and would be subject to criminal penalties. And Blackouts :)";
-      if (result < 0.14) resultMessage = "Getting close to a blackout!";
+    switch (true) {
+      case (result<.02):
+            resultMessage = ("You're sober... no alcohol in your system");
+            break;
+        case (result <= .03 && result >= .02):
+            resultMessage =("You feel mildly relaxed and maybe a little lightheaded. Your inhibitions are slightly loosened, and whatever mood you were in before you started drinking may be mildly intensified.");
+            break;
+        case (result <= .06):
+            resultMessage = ("You feel warm and relaxed. If you're the shy type when you're sober, you lose your feelings of shyness. Your behavior may become exaggerated, making you talk louder or faster or act bolder than usual. Emotions are intensified, so your good moods are better and your bad moods are worse. You may also feel a mild sense of euphoria.");
+            break;
+        case (result <= .09):
+            resultMessage = ("You believe you're functioning better than you actually are. At this level, you may start to slur your speech. Your sense of balance is probably off, and your motor skills are starting to become impaired. Your ability to see and hear clearly is diminished. Your judgment is being affected, so it's difficult for you to decide whether or not to continue drinking. Your ability to evaluate sexual situations is impaired. Students may jokingly refer to this state of mind as beer goggles, but this BAC can have serious repercussions.");
+            break;
+        case (result <= .12):
+            resultMessage = ("At this level, you feel euphoric, but you lack coordination and balance. Your motor skills are markedly impaired, as are your judgment and memory. You probably don't remember how many drinks you've had. Your emotions are exaggerated, and some people become loud, aggressive, or belligerent. If you're a guy, you may have trouble getting an erection when your BAC is this high.");
+            break;
+        case (result < .14):
+            resultMessage = ("At this level, you feel euphoric, but you lack coordination and balance. Your motor skills are markedly impaired, as are your judgment and memory. You probably don't remember how many drinks you've had. Your emotions are exaggerated, and some people become loud, aggressive, or belligerent. If you're a guy, you may have trouble getting an erection when your BAC is this high.");
+            break;
+        case (result <= .17):
+            resultMessage = ("Your euphoric feelings may give way to unpleasant feelings. You have difficulty talking, walking, or even standing.Your judgment and perception are severely impaired. You may become more aggressive, and there is an increased risk of accidentally injuring yourself or others. This is the point when you may experience a blackout.");
+            break;
+        case (result > .17):
+            resultMessage = ("You will probably black out...");
+            break;
+        default:
+            resultMessage = ("You're sober... no alcohol in your system");
+            break;
     }
     handleBac();
   };
